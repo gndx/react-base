3c02a7f994cb49bdf32ec037fbe8339dce5acb1e
Oscar Barajas <oscar@dragons.mx>
2019-01-19 10:57:42 -0500

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     rules: [
       {
