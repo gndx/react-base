ae1f8ce581ddf2a8f8896e16b23305d74cbca90e
Oscar Barajas <oscar@dragons.mx>
2019-06-08 02:24:31 -0500

    [UPDATE] public path

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+      template: './public/index.html',
       filename: './index.html'
     })
   ]
