c4b9ce1461bc302a85b26c39e3b3b72ff6c8ddbd
Wilson Romero <wilson.romero@gmail.com>
2020-04-01 21:29:47 -0500

    Change App to container folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './container/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
